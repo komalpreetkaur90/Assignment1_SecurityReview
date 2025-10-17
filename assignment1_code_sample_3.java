6c9337ef4e18b9937b680529637f1045be231973
komalpreetkaur90 <kkaur209@academic.rrc.ca>
2025-10-17 13:03:06 -0500

    Change API URL from HTTP to HTTPS
    PROBLEM: The app uses regular HTTP, which is not safe.
    RISK: Hackers can see or change the data while it’s being sent.
    FIX: Use HTTPS instead and check the security certificate.

--- assignment1_code_sample_3.java
+++ assignment1_code_sample_3.java
@@ -31,7 +31,7 @@ public class VulnerableApp {
     public static String getData() {
         StringBuilder result = new StringBuilder();
         try {
-            URL url = new URL("http://insecure-api.com/get-data");
+            URL url = new URL("https://secure-api.com/get-data");
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
 
